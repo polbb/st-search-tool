8928fb498ff65d6270146a73913de5b3f2f90ac1
polbb <paulbessone@gmail.com>
2024-03-25 10:13:29 +0000

    second commit

--- main.py
+++ main.py
@@ -21,15 +21,15 @@ dynamodb = boto3.resource('dynamodb', aws_access_key_id=aws_access_key_id, aws_s
 
 st.title("ArgoXai")
 col1, col2, _, _, _, _, _, _ = st.columns([3,3,1,1,1,1,1,1])
-company_number = col1.text_input("Enter CIK code")
-data = st.button("Retrieve Data")
+fuzzy_search = col1.text_input()
+data = st.button("Search")
 
 if data:
     # Ensure the CIK is a string and has leading zeros if necessary
-    cik_str = company_number.zfill(10)
+    st.write(fuzzy_search)
     
-    # Retrieve s3 key from DynamoDB for Risk Factors
-    table = dynamodb.Table('sec_text')
-    response = table.get_item(Key={'companyID': cik_str})
+    # # Retrieve s3 key from DynamoDB for Risk Factors
+    # table = dynamodb.Table('sec_text')
+    # response = table.get_item(Key={'companyID': cik_str})
 
 
