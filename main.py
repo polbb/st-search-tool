b11915ecad99398befddaf247f3a015e60addc91
polbb <paulbessone@gmail.com>
2024-03-25 10:29:58 +0000

    scan table

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import streamlit as st
 import boto3
 
-
-
 # Streamlit UI
 st.set_page_config(layout="wide")  # Force wide mode
-with open( "style.css" ) as css:
-    st.markdown( f'<style>{css.read()}</style>' , unsafe_allow_html= True)
+with open("style.css") as css:
+    st.markdown(f'<style>{css.read()}</style>', unsafe_allow_html=True)
 
 # AWS Credentials
 aws_access_key_id = st.secrets.AWS_ACCESS_KEY_ID
@@ -14,9 +12,7 @@ aws_secret_access_key = st.secrets.AWS_SECRET_ACCESS_KEY
 aws_default_region = st.secrets.AWS_DEFAULT_REGION
 
 # AWS Services Clients
-s3_client = boto3.client('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key, region_name=aws_default_region)
 dynamodb = boto3.resource('dynamodb', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key, region_name=aws_default_region)
-
 
 
 st.title("ArgoXai")
@@ -26,11 +22,26 @@ data = st.button("Search")
 st.divider()
 
 if data:
-    # Ensure the CIK is a string and has leading zeros if necessary
-    st.write(fuzzy_search)
-    
-    # # Retrieve s3 key from DynamoDB for Risk Factors
-    # table = dynamodb.Table('sec_text')
-    # response = table.get_item(Key={'companyID': cik_str})
+    # Display the collected companyIDs
+    table = dynamodb.Table('company_ratios')
 
+    # Initialize empty list to store companyIDs where non_micro == True
+    company_ids = []
+
+    # Initialize the scan
+    response = table.scan(
+        FilterExpression=boto3.dynamodb.conditions.Attr('non_micro').eq(True)
+    )
+
+    # Collect companyIDs
+    company_ids.extend(item['companyID'] for item in response['Items'])
+
+    # Handle pagination if there's more data to scan
+    while 'LastEvaluatedKey' in response:
+        response = table.scan(
+            FilterExpression=boto3.dynamodb.conditions.Attr('non_micro').eq(True),
+            ExclusiveStartKey=response['LastEvaluatedKey']
+        )
+        company_ids.extend(item['companyID'] for item in response['Items'])
+        st.write("Companies with non_micro == True:", company_ids)
 
